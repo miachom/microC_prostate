73a6a83deca012c68337d67086a002f373a75006
Mingkee Achom <40757619+miachom@users.noreply.github.com>
2024-06-27 10:55:46 -0400

    Update chromosight_loop.sh

--- chromatin_loops/chromosight_loop.sh
+++ chromatin_loops/chromosight_loop.sh
@@ -10,7 +10,7 @@ conda init bash
 conda activate /home/ma1111/miniconda3/envs/chromo_env
 
 
-## in cchromisght example -m8000 -M50000 detect 8-80kb loop size with 
+## in chromosight example -m8000 -M50000 detect 8-80kb loop size with 
 out_path=/home/ma1111/microC_project/prostate/compt_analysis/chromosight_run/loop
 seq_path=/home/ma1111/microC_project/prostate/hic_nf_cools/cool_files
 sample_list=('CT20_MicroC_1_S1' , 'CT20_MicroC_2_S2' , 'CT20_MicroC_3_S3' , 'CT20_MicroC_4_S4' , 'CT20_MicroC_5_S5' , 'CT20_MicroC_6_S6' , 'CT20_MicroC_7_S7' , 'CT20_MicroC_8_S8')
